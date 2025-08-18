0175f39210c6d1dd72847eae8cc21f22769da021
agenciaseobrasil <gabriel.arnone@geracaointerativa.com.br>
2025-08-17 23:23:44 -0300

    Fix SWC binary loading issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: false,
+  compiler: {
+    removeConsole: false,
+  },
+  experimental: {
+    forceSwcTransforms: false,
+  },
   experimental: { serverActions: { allowedOrigins: ['*'] } },
 };
 export default nextConfig;
